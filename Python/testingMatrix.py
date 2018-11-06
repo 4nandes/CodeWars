4ef09ad25913cc0dc004bdb49d60af709026f548
Nathan G Fernandes <nfernandes1@csuchico.edu>
2018-11-06 13:59:41 -0800

    fixing stuff up

--- Python/testingMatrix.py
+++
@@ -1,6 +0,0 @@
-# code = ">987v>.v\nv456<  :\n>321 ^ _@"
-# test = [str(x) for x in code.split("\n")]
-# print(test[0][1])
-holder = "tester"
-holder = holder[0:3] + "x" + holder[4:]
-print(holder)
--- Python/weightCheck.py
+++
@@ -1,5 +0,0 @@
-def getWeight(x):
-    return sum(int(n) for n in str(x))
-print(getWeight(23))
-print(getWeight(444))
-print(getWeight(12356))
